9819fa6e5315838b1517bae12752a0d774e5ccba
Etienne Lachere <etiennelachere93@gmail.com>
2020-10-18 23:24:41 +0200

    Spotifetcher models
    * Describe models according to spotify artists endpoint response

--- spotifetcher/models.py
+++ spotifetcher/models.py
@@ -1,11 +1,31 @@
 # -*- coding: utf-8 -*-
 
 from django.db.models import (
-    Model, DateTimeField, EmailField, OneToOneField, CASCADE
+    Model, CharField, IntegerField, ManyToManyField, URLField
 )
+
+
+class Genre(Model):
+    """
+    """
+    name = CharField(max_length=16)
+
+
+class Image(Model):
+    """
+    """
+    height = IntegerField()
+    width = IntegerField()
+    url = URLField()
 
 
 class Artist(Model):
     """
     """
-    pass
+    id = CharField(max_length=32, unique=True, primary_key=True)
+    name = CharField(max_length=32, unique=True)
+    popularity = IntegerField()
+    followers = IntegerField()
+
+    genres = ManyToManyField(Genre)
+    images = ManyToManyField(Image)
